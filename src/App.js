4beebc6a68a1914502a8184c41f6212f790bd214
Carina Ullman <carinalullman@gmail.com>
2020-02-23 17:22:37 -0700

    Added Some Code From Exercise

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
+
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+//import logo from './logo.svg';
+// Import the BrowserRouter, Route and Link components
+import { BrowserRouter, Route, } from 'react-router-dom';
+import Project from './components/project.component.js';
+import Header from './components/header.component.js';
+import Footer from './components/footer.component.js';
+// import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          HELLO WORLD!!! I love you!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+
+        <Route exact path="/" component={Project} />
+        <Route path="/header" component={Header} />
+        <Route path="/footer" component={Footer} />
+        {/* <div className="navigation"> */}
+        {/* <img src={logo} className="logo-image" alt="Logo Image" /> */}
+        {/* <div className="navigation-sub"> */}
+
+        {/* <Link to="/" className="item">Home</Link>
+            <Link to="/header" className="item">Header</Link>
+            <Link to="/footer" className="item">Footer</Link> */}
+
+        {/* </div>
+        </div> */}
+      </div>
+    </BrowserRouter>
   );
 }
 
